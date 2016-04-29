55c2bd44d106b76ea6d57db1d061a84bd15a4d42
Yohanes Lee <hniclee@gmail.com>
2016-04-29 07:52:44 -0400

    course code finsished, still have errors

--- RealWorldYJL/src/edu/saintjoe/cs/yohanesl/RealWorldYJL/Course.java
+++ RealWorldYJL/src/edu/saintjoe/cs/yohanesl/RealWorldYJL/Course.java
@@ -2,9 +2,37 @@ package edu.saintjoe.cs.yohanesl.RealWorldYJL;
 
 public class Course {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		public static final int MAXSTUDENTS = 25;
+		
+		public static String[] classYrNames = {"ImpossibleClassYear", "Freshman", "Sophomore", "Junior", "Senior", "Special"};
+		
+		private String title;
+		private String teacher;
+		private Student[] students;
+		private String room;
+		
+	
+		
+		public Course (String newTitle, String newTeacher, String newRoom) {
+			students = new Student[MAXSTUDENTS];
+			title = newTitle;
+			teacher = newTeacher;
+			room = newRoom;
+		}
+		
+		
+		public String toString() {
+			
+			String output = "";
+			output = "Class information for " + title + " taught by: " + teacher + " meeting in Room: " + room + "\n";
+			
+			for (Student oneStudent: students) {
+				if (oneStudent == null)
+					break;
+				output += oneStudent.getName() + " " + oneStudent.getGender() + " " + classYrNames[oneStudent.getClassYr()] + " " + oneStudent.getAge() + "\n";
+				}
+			return output;
 	}
 
 }
