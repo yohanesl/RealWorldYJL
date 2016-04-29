6bc2c7b7e54e09b0ea6716c707f7571423c6d027
Yohanes Lee <hniclee@gmail.com>
2016-04-29 07:58:25 -0400

    course fully commented

--- RealWorldYJL/src/edu/saintjoe/cs/yohanesl/RealWorldYJL/Course.java
+++ RealWorldYJL/src/edu/saintjoe/cs/yohanesl/RealWorldYJL/Course.java
@@ -2,17 +2,21 @@ package edu.saintjoe.cs.yohanesl.RealWorldYJL;
 
 public class Course {
 
-	
+		// These next two pieces of data are static, and thus NOT instance variables
 		public static final int MAXSTUDENTS = 25;
-		
+		// Create a new array instance and simultaneously assign the first 5 elements
 		public static String[] classYrNames = {"ImpossibleClassYear", "Freshman", "Sophomore", "Junior", "Senior", "Special"};
+		// public static String classYrnames = new String[5];
+		// classyrNames[0] = "ImpossibleClassYr";
 		
+		
+		/* Begin instance variables (aka data members) */
 		private String title;
 		private String teacher;
 		private Student[] students;
 		private String room;
 		
-	
+		/* Data members above, methods below */
 		
 		public Course (String newTitle, String newTeacher, String newRoom) {
 			students = new Student[MAXSTUDENTS];
